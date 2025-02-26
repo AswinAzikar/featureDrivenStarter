2518ddfe3c23999d22527f73bbe0650c2901106b
Aswin Manmathan S <65263396+AswinAzikar@users.noreply.github.com>
2025-02-26 07:04:43 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -14,13 +14,17 @@ cd $project_name
 git clone https://github.com/AswinAzikar/featureDrivenStarter.git
 
 # Copy files from the starter repository
-cp -r starter/lib .
-cp -r starter/assets .
+cp -r featureDrivenStarter/lib .
+cp -r featureDrivenStarter/assets .
 
 # Remove the starter repository
-rm -rf starter
+rm -rf featureDrivenStarter
+
+# Remove starter.sh inside featureDrivenStarter (if it exists)
+rm -f featureDrivenStarter/starter.sh
 
 # Add Flutter packages
-flutter pub add dio dartz flutter_spinkit pinput google_sign_in country_code_picker  firebase_auth  firebase_core firebase_analytics flutter_svg  animations  jwt_decoder  get  hive  path_provider flutter_animate firebase_messaging webview_flutter firebase_crashlytics gap lottie device_info_plus package_info_plus file_picker image_picker image_cropper url_launcher cloud_firestore intl geolocator geocoding dotted_border cached_network_image flutter_typeahead infinite_scroll_pagination fluttertoast dio_cookie_manager auto_size_text connectivity_plus flutter_local_notifications permission_handler google_maps_flutter screen_protector
-code .
+flutter pub add dio dartz flutter_spinkit pinput google_sign_in country_code_picker firebase_auth firebase_core firebase_analytics flutter_svg animations jwt_decoder get hive path_provider flutter_animate firebase_messaging webview_flutter firebase_crashlytics gap lottie device_info_plus package_info_plus file_picker image_picker image_cropper url_launcher cloud_firestore intl geolocator geocoding dotted_border cached_network_image flutter_typeahead infinite_scroll_pagination fluttertoast dio_cookie_manager auto_size_text connectivity_plus flutter_local_notifications permission_handler google_maps_flutter screen_protector
 
+# Open project in VS Code
+code .
